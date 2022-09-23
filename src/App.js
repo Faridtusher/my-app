8d7422083a057c7f62bf7a927c35d0c2c2354f55
Farid <mdfaridulislam509@gmail.com>
2022-09-23 11:25:59 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Numbers></Numbers>
+      <Users></Users>
+      
     </div>
   );
 }
+
+
+function Users (){
+  const [users, setUsers] = useState([]);
+
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  } , [])
+
+  return(
+    <div>  
+     {
+      users.map( user => loadUsers(user))  
+     }
+    </div>
+  )
+}
+
+const loadUsers = (props) =>{
+
+  console.log(props)
+   return(
+    <div>
+      <h3>Name : {props.name}</h3>
+      <p>Email: {props.email} </p>
+      <p>City:{props.address.city} </p>
+    </div>
+   )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Numbers = () => {
+  const [count, setCount] = useState(0);
+
+   const increasesNum = ()=> {
+    let newCount = count + 1;
+    setCount(newCount)
+  }
+
+
+  const decreasesNumber = () => setCount(count - 1)
+ 
+  return(
+    <div>
+      <h1>Number: {count}</h1>
+      <button onClick={increasesNum}>Increases</button>
+      <button onClick={decreasesNumber}>Decreases</button>
+    </div>
+  )
+}
+
+
+
+
+
 
 export default App;
---
+++ src/friend.js
@@ -0,0 +1,19 @@
+function myFriends(){
+
+   const myStyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      margin:'10px',
+      borderRadius:'10px',
+    };
+    
+   return (
+      <div>
+         <div style={myStyle}>
+            <h1>Name: Sakib</h1>
+            <p>Profession: Student</p>
+         </div>
+      </div>
+   )
+}
